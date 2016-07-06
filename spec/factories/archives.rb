78f16b72168bafc7da22a9e279eea5add059f2bd
ypan887 <ypan887@gmail.com>
2016-07-05 17:21:33 -0700

    add factories to create fake archive posts

--- spec/factories/archives.rb
+++ spec/factories/archives.rb
@@ -1,7 +1,4 @@
-FactoryGirl.define do
-  factory :archive do
-    date { Date.today.prev_day }
-    posts { [{"day"=> "#{Date.today.prev_day}",
+fake_post = {"day"=> "#{Date.today.prev_day}",
      "created_at"=>"2016-05-10T00:00:10.000-07:00",
      "tagline" => "this is a test product hunt",
      "name" => "test product",
@@ -10,6 +7,11 @@ FactoryGirl.define do
      "votes_count"=>14,
      "comments_count"=>1,
      "discussion_url"=>
-      "https://www.producthunt.com/podcasts/hustle-41-hiring-an-agency-with-aaron-o-hearn?utm_campaign=producthunt-api&utm_medium=api&utm_source=Application%3A+product+rank+%28ID%3A+2655%29"}] }
+      "some_url"}
+
+FactoryGirl.define do
+  factory :archive do
+    date { Date.today.prev_day }
+    posts { [fake_post] * 2 }
   end
 end
